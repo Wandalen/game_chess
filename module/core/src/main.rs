ea3e36e55e7b10ad4597a1a348bea2539828bdef
volodymyrmigdal <volodymyr.migdal@gmail.com>
2021-12-23 09:12:36 +0200

    core module

---
+++ module/core/src/main.rs
@@ -0,0 +1,7 @@
+
+fn main()
+{
+  let fen = "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2";
+  let board: pleco::Board = pleco::Board::from_fen( fen ).unwrap();
+  board.pretty_print();
+}
