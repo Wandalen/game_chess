07156a750f38ef10d6f37d88c547dd5f5e31bb02
volodymyrmigdal <volodymyr.migdal@gmail.com>
2021-12-24 20:56:14 +0200

    CLI improvements

--- module/cli/src/main.rs
+++ module/cli/src/main.rs
@@ -2,41 +2,44 @@ use game_chess_core::*;
 
 fn main()
 {
-  let mut game: Game = Game::default();
+  let mut game: Option< Game > = None;
   let mut choice;
+
+  command_help();
 
   loop
   {
     println!( "" );
 
-    game.board_print();
-
-    println!( "Turn of {}", game.current_turn() );
-
-    println!( "" );
-
-    println!( "Commands:" );
-
-    println!( "" );
-
-    println!( ".game.new => Create game with default board" );
-    println!( ".move     => Make a move by providing move in UCI format: \"a2a4\" " );
-    println!( ".status   => Print board, current turn, last move" );
-    println!( ".quit     => Exit from the game" );
-
     choice = wca::input::ask( "\nPlease enter command" );
 
     match choice.to_lowercase().trim()
     {
-      ".game.new" => command_game_new( &mut game ),
+      ".game.new" => { game = Some( command_game_new() ) },
       ".move" => command_move( &mut game ),
       ".status" => command_status( &game ),
       ".quit" => command_exit(),
+      ".help" => command_help(),
       command => println!( "Unknown command : {}\n", command ),
     }
 
   }
 
+}
+
+fn command_help()
+{
+  println!( "" );
+
+  println!( "Commands:" );
+
+  println!( "" );
+
+  println!( ".game.new => Create game with default board" );
+  println!( ".move     => Make a move by providing move in UCI format: \"a2a4\" " );
+  println!( ".status   => Print board, current turn, last move" );
+  println!( ".quit     => Exit from the game" );
+  println!( ".help     => Print this help" );
 }
 
 fn command_exit()
@@ -45,13 +48,27 @@ fn command_exit()
   std::process::exit( 0 );
 }
 
-fn command_game_new( game : &mut Game )
+fn command_game_new() -> Game
 {
-  *game = Game::default();
+  let game = Game::default();
+  println!( "" );
+  game.board_print();
+  println!( "Turn of {}", game.current_turn() );
+  game
 }
 
-fn command_status( game : &Game )
+fn command_status( game : &Option<Game> )
 {
+  if game.is_none()
+  {
+    println!( "Create a game first. Use command: .game.new" );
+    return;
+  }
+
+  let game = game.as_ref().unwrap();
+
+  println!( "" );
+
   game.board_print();
 
   println!( "Current turn: {}", game.current_turn() );
@@ -63,11 +80,22 @@ fn command_status( game : &Game )
   }
 }
 
-fn command_move( game : &mut Game )
+fn command_move( game : &mut Option<Game>  )
 {
-  let uci_move = wca::input::ask( "Provide move in UCI format:" );
+  if game.is_none()
+  {
+    println!( "Create a game first. Use command: .game.new" );
+    return;
+  }
+
+  let game = game.as_mut().unwrap();
+
+  let uci_move = wca::input::ask( "Provide move in UCI format, for example 'a2a4'" );
   if !game.make_move( uci_move.as_str() )
   {
     println!( "\n\x1b[93mFailed to apply move: '{}'. Try again!\x1b[0m", uci_move );
   }
+  println!( "" );
+  game.board_print();
+  println!( "Turn of {}", game.current_turn() );
 }
