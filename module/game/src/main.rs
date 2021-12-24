648cb3eff6bfede146dd6a35f506de4f0dc69b09
wandalen <wandalen.me@gmail.com>
2021-12-24 08:17:40 +0200

    styles

--- module/game/src/main.rs
+++ module/game/src/main.rs
@@ -1,4 +1,4 @@
-#![warn(missing_docs)]
+#![warn( missing_docs )]
 
 use game_chess_core as core;
 use bevy::prelude::*;
@@ -21,16 +21,16 @@ fn main()
 
 fn setup
 (
-  mut commands: Commands,
-  asset_server: Res< AssetServer >,
-  mut materials: ResMut<Assets< ColorMaterial > >,
+  mut commands : Commands,
+  asset_server : Res< AssetServer >,
+  mut materials : ResMut<Assets< ColorMaterial > >,
 )
 {
   let texture_handle = asset_server.load( "icon.png" );
   commands.spawn_bundle( OrthographicCameraBundle::new_2d() );
   let sprite = SpriteBundle
   {
-    material: materials.add(texture_handle.into()),
+    material : materials.add(texture_handle.into()),
     ..Default::default()
   };
   commands.spawn_bundle( sprite );
