a504b18cc3175af88485502ba7d719a79db5617e
Pavlo Myroniuk <pspos.developqkation@gmail.com>
2022-01-28 20:26:20 +0200

    Format sample_simple_grpc code; Imrpove workspace structure

--- module/sample_simple_grpc/client/src/generated/mod.rs
+++ module/sample_simple_grpc/client/src/generated/mod.rs
@@ -5,10 +5,10 @@ use std::fmt::{Display, Formatter};
 use crate::generated::chess_example::MoveResultResponse;
 
 impl Display for MoveResultResponse {
-  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-    f.write_str(&format!(
-      "MoveResultResponse {{ status: {}, message: {:?} }}",
-      self.status, self.message
-    ))
-  }
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&format!(
+            "MoveResultResponse {{ status: {}, message: {:?} }}",
+            self.status, self.message
+        ))
+    }
 }
