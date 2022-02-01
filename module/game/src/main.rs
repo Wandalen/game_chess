df04450462acc263b60fc2223964ac05e14aab94
dmvict <dm.vict.kr@gmail.com>
2022-02-01 09:26:52 +0200

    Update drawing system, use implementation of `energycsdx`,

--- module/game/src/main.rs
+++ module/game/src/main.rs
@@ -1,34 +1,201 @@
-#![warn( missing_docs )]
-#![ warn( missing_debug_implementations ) ]
+#![warn(missing_docs)]
+#![warn(missing_debug_implementations)]
 
 //!
 //! Chess game implemented on Bevy for educational purpose.
 //!
 
+use bevy::render::RenderSystem;
+use bevy::render::camera::Camera;
+use bevy::render::camera::CameraProjection;
+use bevy::render::camera::DepthCalculation;
+use bevy::render::camera::VisibleEntities;
+use bevy::render::camera::camera_system;
+use bevy::render::render_graph::base::camera::CAMERA_2D;
 use game_chess_core as core;
 use bevy::prelude::*;
 use bevy::input::system::exit_on_esc_system;
+
+/// The minimal value of `x` that is used in graphics minus half of width of the lefter element.
+/// The value which guarantee that left board rectangles will be shown :
+///
+/// min_board_x - w / 2 = 0 - 1 / 2 = -0.5
+pub const MIN_X : f32 = -0.5;
+
+/// The maximal value of `x` that is used in graphics plus half of width of the righter element.
+/// The value which guarantee that right board rectangles will be shown :
+///
+/// max_board_x + w / 2 = 7 + 1 / 2 = 7.5
+pub const MAX_X : f32 = 7.5;
+
+/// The minimal value of `y` that is used in graphics minus half of height of the bottom element.
+/// The value which guarantee that bottom board rectangles will be shown :
+///
+/// min_board_y - h / 2 = 0 - 1 / 2 = -0.5
+pub const MIN_Y : f32 = -0.5;
+
+/// The maximal value of `y` that is used in graphics plus half of height of the top element.
+/// The value which guarantee that top board rectangles will be shown :
+///
+/// max_board_y + h / 2 = 0 - 1 / 2 = -0.5
+pub const MAX_Y : f32 = 7.5;
+
+/// The constant which allows to center horizontal projection. For only game board it is half of width of the
+/// board rectangle.
+///
+/// w / 2 = 1 / 2 = 0.5
+pub const LEFT_OFFSET : f32 = 0.5;
+
+/// The constant which allows to center vertical projection. For only game board it is half of width of the
+/// board rectangle.
+///
+/// h / 2 = 1 / 2 = 0.5
+pub const TOP_OFFSET : f32 = 0.5;
 
 ///
 /// Main.
 ///
 
-pub fn main()
+#[derive(Debug, Clone, Reflect)]
+#[reflect(Component)]
+pub struct ChessProjection
+{
+  /// offset from left side
+  pub left : f32,
+  /// offset from right side
+  pub right : f32,
+  /// offset from bottom
+  pub bottom : f32,
+  /// offset from top
+  pub top : f32,
+  /// near element for depth sorting
+  pub near : f32,
+  /// far element for depth sorting
+  pub far : f32,
+}
+
+impl CameraProjection for ChessProjection
+{
+  ///
+  /// Transform positions points to projection matrix.
+  ///
+  fn get_projection_matrix(&self) -> Mat4
+  {
+    Mat4::orthographic_rh(self.left, self.right, self.bottom, self.top, self.near, self.far)
+  }
+
+  ///
+  /// Setup positions points taking into account window size and board bounds.
+  ///
+  /// Constant value 8 is the number of board rectangles per dimension.
+  ///
+  fn update(&mut self, width : f32, height : f32)
+  {
+    if width > height
+    {
+      let aspect_ratio = width / height;
+      self.left = (MAX_X - LEFT_OFFSET - 8.0 * aspect_ratio) / 2.0;
+      self.right = (MAX_X + LEFT_OFFSET + 8.0 * aspect_ratio) / 2.0;
+      self.top = MAX_Y;
+      self.bottom = MIN_Y;
+    }
+    else
+    {
+      let aspect_ratio = height / width;
+      self.left = MIN_X;
+      self.right = MAX_X;
+      self.top = (MAX_Y - TOP_OFFSET + 8.0 * aspect_ratio) / 2.0;
+      self.bottom = (MAX_Y - TOP_OFFSET - 8.0 * aspect_ratio) / 2.0;
+    }
+  }
+
+  ///
+  /// Sort entities by depth. Not used.
+  ///
+  fn depth_calculation(&self) -> DepthCalculation { DepthCalculation::Distance }
+}
+
+impl Default for ChessProjection
+{
+  /* Default settings. */
+  fn default() -> Self
+  {
+    ChessProjection {
+      left : -1.0,
+      right : 1.0,
+      bottom : -1.0,
+      top : 1.0,
+      near : 0.0,
+      far : 1000.0,
+    }
+  }
+}
+
+///
+/// Alternative camera bundle that show up the game board.
+///
+
+#[derive(Bundle, Debug)]
+pub struct ChessCameraBundle
+{
+  /// Instance of camera.
+  pub camera : Camera,
+  /// Custom projection.
+  pub chess_projection : ChessProjection,
+  /// Default settings for visible entities.
+  pub visible_entities : VisibleEntities,
+  /// Local transform.
+  pub transform : Transform,
+  /// Global transform.
+  pub global_transform : GlobalTransform,
+}
+
+impl ChessCameraBundle
+{
+  ///
+  /// ChessCameraBundle constructor.
+  ///
+  pub fn new() -> Self
+  {
+    let far = 1000.0;
+    ChessCameraBundle {
+      camera : Camera {
+        name : Some(CAMERA_2D.to_string()),
+        ..Default::default()
+      },
+      chess_projection : ChessProjection {
+        far,
+        ..Default::default()
+      },
+      visible_entities : Default::default(),
+      transform : Transform::from_xyz(0.0, 0.0, far - 0.1),
+      global_transform : Default::default(),
+    }
+  }
+}
+
+fn main()
 {
   let mut app = App::build();
   /* default plugins */
-  app.add_plugins( DefaultPlugins );
+  app.add_plugins(DefaultPlugins);
   /* background */
-  app.insert_resource( ClearColor( Color::rgb( 0.9, 0.9, 0.9 ) ) );
+  app.insert_resource(ClearColor(Color::rgb(0.9, 0.9, 0.9)));
   /* setup core */
-  app.add_startup_system( core_setup.system() );
+  app.add_startup_system(core_setup.system());
   /* setup graphics */
-  app.add_startup_system( graphics_setup.system() );
+  app.add_startup_system(graphics_setup.system());
   /* escape on exit */
-  app.add_system( exit_on_esc_system.system() );
+  app.add_system(exit_on_esc_system.system());
+  app.add_system_to_stage(
+    CoreStage::PostUpdate,
+    camera_system::<ChessProjection>
+      .system()
+      .before(RenderSystem::VisibleEntities),
+  );
   /* for web target */
   #[cfg(target_arch = "wasm32")]
-  app.add_plugin( bevy_webgl2::WebGL2Plugin );
+  app.add_plugin(bevy_webgl2::WebGL2Plugin);
   /* run */
   app.run();
 }
@@ -37,74 +204,40 @@ pub fn main()
 /// Graphics setup.
 ///
 
-pub fn graphics_setup
-(
-  mut commands : Commands,
-  windows : Res< Windows >,
-  mut materials : ResMut< Assets< ColorMaterial > >,
-)
+pub fn graphics_setup(mut commands : Commands, mut materials : ResMut<Assets<ColorMaterial>>)
 {
   /* camera */
-  commands.spawn_bundle( OrthographicCameraBundle::new_2d() );
+  commands.spawn_bundle(ChessCameraBundle::new());
 
-  let window = windows.get_primary().unwrap();
-  let size_in_pixels = ( window.width(), window.height() );
-  let size_in_cells = ( 8, 8 );
-  let size_in_cells_f = ( size_in_cells.0 as f32, size_in_cells.1 as f32 );
-  let side_size = if size_in_pixels.0 < size_in_pixels.1
-  {
-    size_in_pixels.0 / size_in_cells_f.0
-  }
-  else
-  {
-    size_in_pixels.1 / size_in_cells_f.1
-  };
+  let size_in_cells = (8, 8);
 
-  let white = materials.add( ColorMaterial::color( Color::rgb( 0.9, 0.9, 0.7 ) ) );
-  let black = materials.add( ColorMaterial::color( Color::rgb( 0.2, 0.2, 0.1 ) ) );
+  let white = materials.add(ColorMaterial::color(Color::rgb(0.9, 0.9, 0.7)));
+  let black = materials.add(ColorMaterial::color(Color::rgb(0.2, 0.2, 0.1)));
 
   for x in 0 .. size_in_cells.0
   {
     for y in 0 .. size_in_cells.1
     {
+      let material = if (x + y) % 2 == 0 { black.clone() } else { white.clone() };
 
-      let material = if ( x + y ) % 2 == 0
-      {
-        white.clone()
-      }
-      else
-      {
-        black.clone()
+      let sprite = Sprite {
+        size : Vec2::new(1.0, 1.0),
+        ..Default::default()
       };
 
-      let sprite = Sprite
-      {
-        size : Vec2::new( side_size, side_size ),
-        .. Default::default()
+      let transform = Transform {
+        translation : Vec3::new(x as f32, y as f32, 0.),
+        ..Default::default()
       };
 
-      let transform = Transform
-      {
-        translation : Vec3::new
-        (
-          ( x as f32 - size_in_cells_f.0 / 2. + 0.5 ) * side_size,
-          ( y as f32 - size_in_cells_f.0 / 2. + 0.5 ) * side_size,
-          0.
-        ),
-        .. Default::default()
-      };
-
-      commands.spawn_bundle( SpriteBundle
-      {
+      commands.spawn_bundle(SpriteBundle {
         sprite,
         material,
         transform,
-        .. Default::default()
+        ..Default::default()
       });
-
     }
   }
-
 }
 
 ///
@@ -115,6 +248,6 @@ pub fn core_setup()
 {
   let mut game = core::Game::default();
   game.board_print();
-  game.make_move( "a2a4" );
+  game.make_move("a2a4");
   game.board_print();
 }
