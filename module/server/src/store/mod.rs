a84bba03667643ded0cfe7aff2907c6546a6a192
Anastasiia Lukianenko <vicooodin@gmail.com>
2022-02-06 15:37:34 +0200

    [multiplayer] Add push_game_gg function
    Must be implemented:
    - connection with command and server
    - sending message to chat (push mgs functionality).
    
    Signed-off-by: Anastasiia Lukianenko <vicooodin@gmail.com>

--- module/server/src/store/mod.rs
+++ module/server/src/store/mod.rs
@@ -10,11 +10,11 @@ use multiplayer::MultiplayerGame;
 pub trait GameStore
 {
   /// Add game to storage.
-  async fn add_game(&mut self, game : MultiplayerGame);
+  fn add_game(&mut self, game : MultiplayerGame);
   /// Get game from storage by string ( slice ) id.
-  async fn get_game(&self, game_id : &str) -> &MultiplayerGame;
+  fn get_game(&self, game_id : &str) -> &MultiplayerGame;
   /// Get all stored games.
-  async fn get_games(&self) -> &Vec<MultiplayerGame>;
+  fn get_games(&self) -> &Vec<MultiplayerGame>;
   /// Update game in storage using string id and new instance of Game.
-  async fn update_game(&mut self, game_id : &str, new_game : MultiplayerGame);
+  fn update_game(&mut self, game_id : &str, new_game : MultiplayerGame);
 }
