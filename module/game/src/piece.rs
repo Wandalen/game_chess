296e92a1137d637506bfc839b2d27f97977cdf8f
wandalen <wandalen.me@gmail.com>
2022-02-06 12:57:21 +0200

    cleanup

--- module/game/src/piece.rs
+++ module/game/src/piece.rs
@@ -7,24 +7,12 @@
 
 use bevy::prelude::*;
 use game_chess_core as core;
-use crate::core::Piece;
-
-///
-/// Piece texture atlas
-///
-
-type PieceToTexture = std::collections::HashMap<u8, u8>;
 
 ///
 /// Pieces drawing system
 ///
 
-pub fn pieces_setup(
-  mut commands : Commands,
-  asset_server : Res<AssetServer>,
-  mut texture_atlases : ResMut<Assets<TextureAtlas>>,
-  game : Res<core::Game>,
-)
+pub fn pieces_setup( mut commands : Commands )
 {
 
 }
