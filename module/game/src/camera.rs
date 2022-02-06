dcdd2be545211807c23f785ded938874c23385b1
wandalen <wandalen.me@gmail.com>
2022-02-06 10:22:08 +0200

    missing file camera

---
+++ module/game/src/camera.rs
@@ -0,0 +1,135 @@
+#![warn(missing_docs)]
+#![warn(missing_debug_implementations)]
+
+//!
+//! Chess game implemented on Bevy for educational purpose.
+//!
+
+use bevy::prelude::*;
+use bevy::render::camera::Camera;
+use bevy::render::camera::CameraProjection;
+use bevy::render::camera::DepthCalculation;
+use bevy::render::camera::VisibleEntities;
+use bevy::render::render_graph::base::camera::CAMERA_2D;
+
+///
+/// Projection.
+///
+
+#[derive(Debug, Clone, Reflect)]
+#[reflect(Component)]
+pub struct ChessProjection
+{
+  /// offset from left side
+  pub left : f32,
+  /// offset from right side
+  pub right : f32,
+  /// offset from bottom
+  pub bottom : f32,
+  /// offset from top
+  pub top : f32,
+  /// near element for depth sorting
+  pub near : f32,
+  /// far element for depth sorting
+  pub far : f32,
+}
+
+impl CameraProjection for ChessProjection
+{
+  ///
+  /// Transform positions points to projection matrix.
+  ///
+  fn get_projection_matrix(&self) -> Mat4
+  {
+    Mat4::orthographic_rh(self.left, self.right, self.bottom, self.top, self.near, self.far)
+  }
+
+  ///
+  /// Setup projection points taking into account window size and board bounds.
+  ///
+  fn update(&mut self, width : f32, height : f32)
+  {
+    if width > height
+    {
+      /* if width > height we need to shrink left and right sides by delta */
+      let delta = width / height - 1.0;
+      self.left = -1.0 - delta;
+      self.right = 1.0 + delta;
+      self.top = 1.0;
+      self.bottom = -1.0;
+    }
+    else
+    {
+      /* if width > height we need to shrink bottom and top by delta */
+      let delta = height / width - 1.0;
+      self.left = -1.0;
+      self.right = 1.0;
+      self.top = 1.0 + delta;
+      self.bottom = -1.0 - delta;
+    }
+  }
+
+  ///
+  /// Sort entities by depth. Not used.
+  ///
+  fn depth_calculation(&self) -> DepthCalculation { DepthCalculation::Distance }
+}
+
+impl Default for ChessProjection
+{
+  /* Default settings. */
+  fn default() -> Self
+  {
+    ChessProjection {
+      left : -1.0,
+      right : 1.0,
+      bottom : -1.0,
+      top : 1.0,
+      near : 0.0,
+      far : 1000.0,
+    }
+  }
+}
+
+///
+/// Alternative camera bundle that show up the game board.
+///
+
+#[derive(Bundle, Debug)]
+pub struct ChessCameraBundle
+{
+  /// Instance of camera.
+  pub camera : Camera,
+  /// Custom projection.
+  pub chess_projection : ChessProjection,
+  /// Default settings for visible entities.
+  pub visible_entities : VisibleEntities,
+  /// Local transform.
+  pub transform : Transform,
+  /// Global transform.
+  pub global_transform : GlobalTransform,
+}
+
+impl ChessCameraBundle
+{
+  ///
+  /// ChessCameraBundle constructor.
+  ///
+  pub fn new() -> Self
+  {
+    let far = 1000.0;
+    ChessCameraBundle {
+      camera : Camera {
+        name : Some(CAMERA_2D.to_string()),
+        ..Default::default()
+      },
+      chess_projection : ChessProjection {
+        far,
+        ..Default::default()
+      },
+      visible_entities : Default::default(),
+      transform : Transform::from_xyz(0.0, 0.0, far - 0.1),
+      global_transform : Default::default(),
+    }
+  }
+}
