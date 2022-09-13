ddcbb8848e33aa2f42873d80a76ba2daba1b8835
dmvict <dm.vict.kr@gmail.com>
2022-09-13 13:19:24 +0300

    Update sample `sprite`, use new version of engine `bevy`

--- sample/sprite/src/main.rs
+++ sample/sprite/src/main.rs
@@ -1,9 +1,8 @@
-#![warn(missing_docs)]
+#![ warn( missing_docs ) ]
 
 //! Sample shows how to add sprite in Bevy.
 
 use bevy::prelude::*;
-use bevy::render::pass::ClearColor;
 
 const DISPLAY_HEIGHT : f32 = 300.0;
 const DISPLAY_WIDTH : f32 = 300.0;
@@ -12,30 +11,30 @@ const DISPLAY_WIDTH : f32 = 300.0;
 
 fn main()
 {
-  App::build()
-    .insert_resource(ClearColor(Color::rgb(0.9, 0.9, 0.9)))
-    .insert_resource(WindowDescriptor {
-      title : "Draw sprite".to_string(),
-      width : DISPLAY_WIDTH,
-      height : DISPLAY_HEIGHT,
-      resizable : false,
-      ..Default::default()
-    })
-    .add_plugins(DefaultPlugins)
-    .add_startup_system(setup.system())
-    .run();
+  App::new()
+  .insert_resource( ClearColor( Color::rgb( 0.9, 0.9, 0.9 ) ) )
+  .insert_resource( WindowDescriptor
+  {
+    title : "Draw sprite".to_string(),
+    width : DISPLAY_WIDTH,
+    height : DISPLAY_HEIGHT,
+    resizable : false,
+    .. Default::default()
+  })
+  .add_plugins( DefaultPlugins )
+  .add_startup_system( setup )
+  .run();
 }
 
 //
 
-fn setup(mut commands : Commands, asset_server : Res<AssetServer>, mut materials : ResMut<Assets<ColorMaterial>>)
+fn setup( mut commands : Commands, asset_server : Res< AssetServer > )
 {
-  // loading sprite from image
-  let texture_handle = asset_server.load("icon.png");
-  commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+  commands.spawn_bundle( Camera2dBundle::default() );
   // adding sprite
-  commands.spawn_bundle(SpriteBundle {
-    material : materials.add(texture_handle.into()),
-    ..Default::default()
+  commands.spawn_bundle( SpriteBundle
+  {
+    texture : asset_server.load( "icon.png" ),
+    .. Default::default()
   });
 }
