1f858018fb8500a10c8d81a38e8396f4dc4afdd7
Holger <ollgert@gmail.com>
2023-03-10 15:26:26 +0200

    NonSend

--- module/core/src/lib.rs
+++ module/core/src/lib.rs
@@ -35,7 +35,6 @@ use serde::{ Serialize, Deserialize, Serializer, Deserializer };
 use rand::Rng;
 use rand::thread_rng;
 
-use bevy::prelude::Resource;
 
 /* Structure:
 
@@ -498,7 +497,7 @@ pub enum GameStatus
 /// Basically Board + History.
 ///
 
-#[ derive( Serialize, Deserialize, Debug, Resource, ) ]
+#[ derive( Serialize, Deserialize, Debug, ) ]
 pub struct Game
 {
   #[ serde( serialize_with = "board_ser", deserialize_with = "board_der" ) ]
